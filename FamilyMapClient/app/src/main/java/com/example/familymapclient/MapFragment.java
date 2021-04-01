a128a61a693f2fd145998bb6d8875189e0af2890
Dan Pettingill <dptres16@gmail.com>
2021-04-01 10:19:26 -0600

    Client a cleaner version of the last commit

--- FamilyMapClient/app/src/main/java/com/example/familymapclient/MapFragment.java
+++ FamilyMapClient/app/src/main/java/com/example/familymapclient/MapFragment.java
@@ -65,36 +65,17 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleM
         Datacache instance = Datacache.getInstance();
         int[] markerColors = {0, 240, 120, 30, 270, 150, 60, 90, 180, 210, 300, 330}; //color wheel pts. 0 == red, 120 == green, 240 == blue
         int index = 0;
-        int iteration = 0;
-
 
         for (Map.Entry<String, Set<Event>> entry : instance.getEventsMap().entrySet()) {
             for (Event e : entry.getValue()) {
                 index = instance.getEventTypes().get(e.getEventType()); //get the appropriate color index for this marker
-                System.out.println(iteration);
                 if (index > markerColors.length - 1) //if the index is too high
                     index -= markerColors.length - 1; //reset it to an in-bounds value
                 LatLng position = new LatLng(e.getLatitude(), e.getLongitude()); //get pos of this marker
-//                Drawable icon = new IconDrawable(getActivity(), FontAwesomeIcons.fa_map_marker).
-//                        colorRes(markerColors[index]).sizeDp(40);
-//                icon.setAlpha(255);
-
-////                if (iteration == 0)
-////                {
-//                    bitmap = Bitmap.createBitmap(icon.getIntrinsicWidth(), icon.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-//                    canvas.setBitmap(bitmap);
-//                    bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
-////                }
-//                Bitmap bitmap = Bitmap.createBitmap(icon.getIntrinsicWidth(), icon.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);;
-//                Canvas canvas = new Canvas();
-//                canvas.setBitmap(bitmap);
-//                BitmapDescriptor bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(bitmap);
-//                icon.draw(canvas);
 
                 MarkerOptions marker = new MarkerOptions()
-                        .icon(BitmapDescriptorFactory.defaultMarker(359)).position(position).title(e.getEventType());
+                        .icon(BitmapDescriptorFactory.defaultMarker(markerColors[index])).position(position).title(e.getEventType());
                 map.addMarker(marker);
-                iteration++;
 //                map.setOnMarkerClickListener(this);
             }
         }
